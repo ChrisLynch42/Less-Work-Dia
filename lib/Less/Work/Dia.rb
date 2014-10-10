7cbf0a96bb930a8d3469022d68e18c95735c29fe
Chris Lynch <chrislynch42@yahoo.com>
2014-10-10 00:09:42 -0400

    Initial Gem setup

---
+++ lib/Less/Work/Dia.rb
@@ -0,0 +1,9 @@
+require "Less/Work/Dia/version"
+
+module Less
+  module Work
+    module Dia
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/Less/Work/Dia/version.rb
@@ -0,0 +1,7 @@
+module Less
+  module Work
+    module Dia
+      VERSION = "0.0.1"
+    end
+  end
+end
