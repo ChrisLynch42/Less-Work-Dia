ae3d513042f73fa362ed3089373a9362eb30303d
Chris Lynch <chrislynch42@yahoo.com>
2014-10-15 17:22:42 -0400

    Moved class to Less::Work::Dia module.

--- lib/Less/Work/Dia/Database/dia_database_file.rb
+++ lib/Less/Work/Dia/Database/dia_database_file.rb
@@ -1,25 +1,32 @@
 require 'nokogiri'
-require_relative '../base_mixin'
-
-class DiaDatabaseFile
-  include BaseMixin
-
-  def read_file(parameters = {})
-    parameters_pair_nil?(parameters,:file_path)
-    parse_file_content(open_file(parameters[:file_path]))
-  end
+require_relative '../../base_mixin'
 
 
-  private
-  def open_file(file_path)
-    if File.exist?(file_path)
-      f = File.open(file_path)
-    end
-  end
+module Less
+  module Work
+    module Dia
+      class DiaDatabaseFile
+        include BaseMixin
 
-  def parse_file_content(file)
-    if !file.nil?
-      dia_xml = Nokogiri::XML(file)
+        def read_file(parameters = {})
+          parameters_pair_nil?(parameters,:file_path)
+          parse_file_content(open_file(parameters[:file_path]))
+        end
+
+
+        private
+        def open_file(file_path)
+          if File.exist?(file_path)
+            f = File.open(file_path)
+          end
+        end
+
+        def parse_file_content(file)
+          if !file.nil?
+            dia_xml = Nokogiri::XML(file)
+          end
+        end
+      end
     end
   end
 end
