4cc62de505965d01049b5108e630e7d36c3549d8
Chris Lynch <chrislynch42@yahoo.com>
2014-10-15 17:16:36 -0400

    Made it a submodule of Less::Work

--- lib/Less/Work/base_mixin.rb
+++ lib/Less/Work/base_mixin.rb
@@ -1,21 +1,26 @@
-module BaseMixin
-  def parameters_nil?(parameters)
-    if parameters.nil? || !parameters.is_a?(::Hash)
-      raise ArgumentError, 'Missing parameters hash.', caller
+
+module Less
+  module Work
+    module BaseMixin
+      def parameters_nil?(parameters)
+        if parameters.nil? || !parameters.is_a?(::Hash)
+          raise ArgumentError, 'Missing parameters hash.', caller
+        end
+      end
+
+      def parameters_pair_nil?(parameters,key)
+        parameters_nil?(parameters)
+        pair_nil?(parameters,key)
+      end
+
+      def pair_nil?(parameters,key)
+        if parameters[key].nil?
+          raise ArgumentError, 'Missing value for key ' + key.to_s, caller
+        end
+      end
+
+
+
     end
   end
-
-  def parameters_pair_nil?(parameters,key)
-    parameters_nil?(parameters)
-    pair_nil?(parameters,key)
-  end
-
-  def pair_nil?(parameters,key)
-    if parameters[key].nil?
-      raise ArgumentError, 'Missing value for key ' + key.to_s, caller
-    end
-  end
-
-
-
 end
