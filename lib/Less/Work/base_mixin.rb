d78447cf6a55146a7f215697f5e9f7519f2571b9
Chris Lynch <chrislynch42@yahoo.com>
2014-10-16 22:41:15 -0400

    parameters_hash? needed to return hash.

--- lib/Less/Work/base_mixin.rb
+++ lib/Less/Work/base_mixin.rb
@@ -4,14 +4,14 @@ module Less
     module BaseMixin
       def parameters_hash?(parameters)
         begin
-          parameters = parameters.to_h
+          parameters.to_h
         rescue
           raise ArgumentError, 'Parameter should be Hash or implement #to_h.', caller
         end
       end
 
       def parameters_pair_nil?(parameters,key)
-        parameters_hash?(parameters)
+        parameters = parameters_hash?(parameters)
         parameters.fetch(key) do
           raise ArgumentError, 'Parameter should be hash containing key ' + key.to_s, caller
         end
