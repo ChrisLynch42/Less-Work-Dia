b3ccfb109a17a7147092f6ba132a9a94b12879b8
Chris Lynch <chrislynch42@yahoo.com>
2014-10-17 23:19:33 -0400

    Connection point specific to references

---
+++ lib/Less/Work/Dia/Database/reference_connection_point.rb
@@ -0,0 +1,32 @@
+require_relative '../connection_point'
+
+module Less
+  module Work
+    module Dia
+      class ReferenceConnectionPoint < ConnectionPoint
+        attr_accessor :column_name, :table_name
+
+        def initialize()
+          @translate_hash = Hash.new()
+          (12..200).each{ | val |
+            evenVal = val - (val % 2)
+            row = (evenVal - 10)
+            row = row / 2
+            @translate_hash[val]=row;
+            @translate_hash[val.to_s]=row;
+
+          }
+        end
+
+        def column_order
+          connection_to_column_order(@connection)
+        end
+
+        def connection_to_column_order(connection_point)
+          @translate_hash[connection_point]
+        end
+
+      end
+    end
+  end
+end
