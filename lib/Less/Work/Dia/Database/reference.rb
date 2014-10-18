0944e27fef953b32daff38002fc2093a5b43cca9
Chris Lynch <chrislynch42@yahoo.com>
2014-10-17 23:20:30 -0400

    Added attributes

--- lib/Less/Work/Dia/Database/reference.rb
+++ lib/Less/Work/Dia/Database/reference.rb
@@ -2,6 +2,8 @@ module Less
   module Work
     module Dia
       class Reference
+        attr_accessor :start_point, :end_point, :id
+
       end
     end
   end
--- lib/Less/Work/Dia/Database/table.rb
+++ lib/Less/Work/Dia/Database/table.rb
@@ -2,6 +2,7 @@ module Less
   module Work
     module Dia
       class Table
+        attr_accessor :name, :columns, :columns_in_order, :references, :type
       end
     end
   end
