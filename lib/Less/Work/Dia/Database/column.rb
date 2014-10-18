c4181a591bac72d2d9aba180a3983f59d113b842
Chris Lynch <chrislynch42@yahoo.com>
2014-10-17 23:20:05 -0400

    Basic table column

---
+++ lib/Less/Work/Dia/Database/column.rb
@@ -0,0 +1,10 @@
+module Less
+  module Work
+    module Dia
+      class Column
+        attr_accessor :name, :data_type, :primary_key, :nullable, :unique, :comment
+
+      end
+    end
+  end
+end
