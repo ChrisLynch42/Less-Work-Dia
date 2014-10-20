9804d4b6dbd75ae8746786c1b9a4e7fab552bf2b
Chris Lynch <chrislynch42@yahoo.com>
2014-10-20 19:14:31 -0400

    Added diagram id

--- lib/Less/Work/Dia/Database/table.rb
+++ lib/Less/Work/Dia/Database/table.rb
@@ -2,7 +2,7 @@ module Less
   module Work
     module Dia
       class Table
-        attr_accessor :name, :columns, :columns_in_order, :references, :type
+        attr_accessor :name, :columns, :columns_in_order, :references, :type, :diagram_id
       end
     end
   end
