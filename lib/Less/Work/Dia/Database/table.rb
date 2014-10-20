812867068878f64571fff60425ad89a1a9af128c
Chris Lynch <chrislynch42@yahoo.com>
2014-10-20 19:36:34 -0400

    Initialize collections as hashes.

--- lib/Less/Work/Dia/Database/table.rb
+++ lib/Less/Work/Dia/Database/table.rb
@@ -2,7 +2,14 @@ module Less
   module Work
     module Dia
       class Table
-        attr_accessor :name, :columns, :columns_in_order, :references, :type, :diagram_id
+        attr_accessor :name, :type, :diagram_id
+        attr_reader :columns, :columns_in_order, :references
+
+        def initialize
+          self.columns = Hash.new()
+          self.columns_in_order = Hash.new()
+          self.references = Hash.new()
+        end
       end
     end
   end
