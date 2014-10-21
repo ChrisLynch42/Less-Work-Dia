e69afa9472c68bba12b33a5f55d8acc03d2f89d5
Chris Lynch <chrislynch42@yahoo.com>
2014-10-20 20:20:18 -0400

    Added file to database module.  Also added reference parsing methods.

--- lib/Less/Work/Dia/Database/diagram_parser.rb
+++ lib/Less/Work/Dia/Database/diagram_parser.rb
@@ -9,70 +9,95 @@ require_relative 'diagram_object'
 module Less
   module Work
     module Dia
-      class DiagramParser
-        include ParameterMixin
-        include ObjectParserMixin
+      module Database
+        class DiagramParser
+          include ParameterMixin
+          include ObjectParserMixin
 
-        attr_reader :diagram_object
+          attr_reader :diagram_object
 
 
-        def initialize(parameters = {})
-          self.diagram_object=DiagramObject.new()
-          parse(parameters);
-        end
+          def initialize(parameters = {})
+            self.diagram_object=DiagramObject.new()
+            parse(parameters);
+          end
 
-        private
-        attr_writer :diagram_object
+          private
+          attr_writer :diagram_object
 
-        def parse(parameters)
-          parameters_pair_check parameters, :diagram_xml
-          parse_tables(parameters[:diagram_xml])
-          parse_references(parameters[:diagram_xml])
-        end
+          def parse(parameters)
+            parameters_pair_check parameters, :diagram_xml
+            parse_tables(parameters[:diagram_xml])
+            parse_references(parameters[:diagram_xml])
+          end
 
-        def parse_references(diagram_xml)
-          reference_nodes = diagram_xml.xpath("//dia:object[@type='Database - Reference']")
-          if !reference_nodes.nil?
+          def parse_references(diagram_xml)
+            reference_nodes = diagram_xml.xpath("//dia:object[@type='Database - Reference']")
+            if !reference_nodes.nil?
 
-            reference_nodes.each do |reference_node|
-              reference = Reference.new()
-              diagram_object.references[reference.diagram_id] = reference
+              reference_nodes.each do |reference_node|
+                reference = Reference.new()
+                reference.diagram_id = reference_node.attr('id').to_s
+                diagram_object.references[reference.diagram_id] = reference
+              end
             end
           end
 
-        end
+          def parse_reference_endpoints(reference_node)
+            connection_xpath = "./dia:connections/dia:connection"
+            connection_nodes = target_node.xpath(connection_xpath)
+            if !connection_nodes.nil?
+              connection_nodes.each { |side|
+                if !side.nil?
+                  if side['handle'] == '0'
+                    set_reference_point_values(reference_node.start_point, side)
+                  else
+                    set_reference_point_values(reference_node.end_point, side)
+                  end
+                end
+              }
+            end
+          end
 
-        def parse_tables(diagram_xml)
-          table_nodes = diagram_xml.xpath("//dia:object[@type='Database - Table']")
-          if !table_nodes.nil?
-            table_nodes.each do |table_node|
-              table = Table.new()
-              table.name=get_dia_string(table_node,'name')
-              parse_table_columns(table_node,table)
-              diagram_object.tables[table.name] = table
-              diagram_object.tables_by_id[table.diagram_id] = table
+          def set_reference_point_values(target, side)
+            target.handle=side['handle']
+            target.target_object_id=side['to']
+            target.connection=side['connection']
+          end
+
+
+
+          def parse_tables(diagram_xml)
+            table_nodes = diagram_xml.xpath("//dia:object[@type='Database - Table']")
+            if !table_nodes.nil?
+              table_nodes.each do |table_node|
+                table = Table.new()
+                table.name=get_dia_string(table_node, 'name')
+                parse_table_columns(table_node, table)
+                diagram_object.tables[table.name] = table
+                diagram_object.tables_by_id[table.diagram_id] = table
+              end
+            end
+          end
+
+          def parse_table_columns(table_node, table)
+            column_nodes = table_node.xpath("./dia:attribute[@name='attributes']/dia:composite[@type='table_attribute']")
+            if !column_nodes.nil?
+              column_nodes.each do |column_node|
+                column = Column.new()
+                column.name=get_dia_string(column_node, 'name')
+                column.data_type = get_dia_string(column_node, 'type')
+                column.comment = get_dia_string(column_node, 'comment')
+                column.primary_key = get_dia_boolean(column_node, 'primary_key')
+                column.nullable = get_dia_boolean(column_node, 'nullable')
+                column.unique = get_dia_boolean(column_node, 'unique')
+
+                table.columns[column.name] = column
+                table.columns_in_order[table.columns_in_order.length] = column
+              end
             end
           end
         end
-
-        def parse_table_columns(table_node,table)
-          column_nodes = table_node.xpath("./dia:attribute[@name='attributes']/dia:composite[@type='table_attribute']")
-          if !column_nodes.nil?
-            column_nodes.each do |column_node|
-              column = Column.new()
-              column.name=get_dia_string(column_node,'name')
-              column.data_type = get_dia_string(column_node,'type')
-              column.comment = get_dia_string(column_node,'comment')
-              column.primary_key = get_dia_boolean(column_node,'primary_key')
-              column.nullable = get_dia_boolean(column_node,'nullable')
-              column.unique = get_dia_boolean(column_node,'unique')
-
-              table.columns[column.name] = column
-              table.columns_in_order[table.columns_in_order.length] = column
-            end
-          end
-        end
-
 
       end
     end
