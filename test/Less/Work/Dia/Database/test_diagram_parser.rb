ad0a3d90a34ab4ecc56967265d8b3d4726704f00
Chris Lynch <chrislynch42@yahoo.com>
2014-10-21 00:21:18 -0400

    Added test to validate the database object returned from DiagramParser.

--- test/Less/Work/Dia/Database/test_diagram_parser.rb
+++ test/Less/Work/Dia/Database/test_diagram_parser.rb
@@ -28,6 +28,19 @@ module Less
             assert_silent { diagram_parser = return_valid_diagram_parser_object }
           end
 
+          def test_diagram_parser_diagram_object
+            diagram_parser = return_valid_diagram_parser_object
+            assert_equal(27, diagram_parser.diagram_object.tables.length,"Diagram object table count was not 27.")
+            assert_equal(28, diagram_parser.diagram_object.references.length,"Diagram object reference count was not 28.")
+            refute_nil(diagram_parser.diagram_object.tables['spells'],"Diagram object Spells table was missing.")
+            assert_equal('spells', diagram_parser.diagram_object.tables['spells'].name, "Diagram object spells table's name was not 'spells'.")
+            assert_equal(11, diagram_parser.diagram_object.tables['spells'].columns.length, "Diagram object spells table column count was not 11.")
+            refute_nil(diagram_parser.diagram_object.tables['spells'].columns['id'],"Diagram object Spells table id column was missing.")
+            assert_equal('id', diagram_parser.diagram_object.tables['spells'].columns['id'].name, "Diagram object spells table's id column's name was not 'id'.")
+            assert_equal(true, diagram_parser.diagram_object.tables['spells'].columns['id'].primary_key, "Diagram object spells table's id column's was not a primary key.")
+
+          end
+
 
           private
           def return_valid_diagram_file_object
