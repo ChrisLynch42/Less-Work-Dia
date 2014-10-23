13a680416a7b3dcb4eaa1bbee712f339becb11aa
Chris Lynch <chrislynch42@yahoo.com>
2014-10-22 21:26:26 -0400

    Added class to Database module

--- lib/Less/Work/Dia/Database/column.rb
+++ lib/Less/Work/Dia/Database/column.rb
@@ -1,9 +1,11 @@
 module Less
   module Work
     module Dia
-      class Column
-        attr_accessor :name, :data_type, :primary_key, :nullable, :unique, :comment
+      module Database
+        class Column
+          attr_accessor :name, :data_type, :primary_key, :nullable, :unique, :comment
 
+        end
       end
     end
   end
