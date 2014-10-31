d1d56133259c251926eeb01907006c49972458c2
Chris Lynch <chrislynch42@yahoo.com>
2014-10-31 02:07:31 -0400

    XMLFragment will be the adapter which contains a node

---
+++ lib/Less/Work/Dia/Database/xml_fragment.rb
@@ -0,0 +1,33 @@
+require_relative '../../parameter_mixin'
+
+module Less
+  module Work
+    module Dia
+      module Database
+        class XMLFragment
+          include ParameterMixin
+          attr_reader :diagram_fragment
+          attr_reader :xml
+
+          def initialize(parameters = {})
+            parameters_pair_check(parameters,:diagram_fragment);
+            self.diagram_fragment = parameters[:diagram_fragment]
+            self.xml=namespaced_fragment(self.diagram_fragment)
+          end
+
+
+          private
+          attr_writer :diagram_fragment
+          attr_writer :xml
+
+          def namespaced_fragment(diagram_fragment)
+            xml_doc = Nokogiri::XML(%Q(<root xmlns:dia='http://www.lysator.liu.se/~alla/dia/'/>))
+            xml_doc.fragment(diagram_fragment)
+          end
+
+
+        end
+      end
+    end
+  end
+end
---
+++ test/Less/Work/Dia/Database/test_xml_fragment.rb
@@ -0,0 +1,28 @@
+require 'minitest/autorun'
+require_relative '../../../../../lib/Less/Work/Dia/Database/xml_fragment'
+
+module Less
+  module Work
+    module Dia
+      module Database
+        class TestXMLFragment < Minitest::Test
+
+          # Called before every test method runs. Can be used
+          # to set up fixture information.
+          def setup
+            # Do nothing
+          end
+
+          # Called after every test method runs. Can be used to tear
+          # down fixture information.
+
+          def teardown
+            # Do nothing
+          end
+
+
+        end
+      end
+    end
+  end
+end
