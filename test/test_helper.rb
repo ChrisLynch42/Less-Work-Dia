3a3dc5d138ae029b25dc06f02f461fc9b46ba15f
Chris Lynch <chrislynch42@yahoo.com>
2014-11-03 21:12:56 -0500

    Changed start directory for tests to accommodate running tests from command line as well as rubymine

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,5 @@
 class TestFiles
-  TEST_FILE_DIRECTORY = Dir.pwd + '/test_files'
+  TEST_FILE_DIRECTORY = Dir.pwd + '/test/test_files'
   MASTER_SLAVE = TEST_FILE_DIRECTORY + '/master_slave.dia'
   TEST = TEST_FILE_DIRECTORY + '/test.dia'
 
