85e738fe5eca03bbc04238f490d460d916c522bd
Chris Lynch <chrislynch42@yahoo.com>
2015-04-21 16:15:44 -0400

    Added relationship class for communicating table relationships.

---
+++ lib/Less/Work/Dia/Database/relationship.rb
@@ -0,0 +1,14 @@
+require_relative 'relationship_mixin'
+
+module Less
+  module Work
+    module Dia
+      module Database
+        class Relationship
+          include RelationshipMixin
+
+        end
+      end
+    end
+  end
+end
