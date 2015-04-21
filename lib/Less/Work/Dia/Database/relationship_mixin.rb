c142cd236a116ba8b0da50fd98e79c1f6e15e46a
Chris Lynch <chrislynch42@yahoo.com>
2015-04-21 16:07:38 -0400

    Added relationship and relationship mixin for communicating database relationships.

---
+++ lib/Less/Work/Dia/Database/relationship_mixin.rb
@@ -0,0 +1,12 @@
+module Less
+  module Work
+    module Dia
+      module Database
+        module RelationshipMixin
+          attr_accessor :parent_table, :referenced_key_column, :child_table, :foreign_key_column
+
+        end
+      end
+    end
+  end
+end
