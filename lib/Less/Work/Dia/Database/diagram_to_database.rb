c3c188bcc2c2237cc0b9f7705e40213a1e50e2b7
Chris Lynch <chrislynch42@yahoo.com>
2015-04-28 12:07:29 -0400

    Added code to find relationships.

--- lib/Less/Work/Dia/Database/diagram_to_database.rb
+++ lib/Less/Work/Dia/Database/diagram_to_database.rb
@@ -29,13 +29,19 @@ module Less
 
             self.diagram_reader.database_diagram_members.references.each do | reference_id, reference |
               relationship = Relationship.new()
-              table_parent = self.diagram_reader.database_diagram_members.tables[reference.start_point.target_object_id]
-              #relationship.parent_table = table_parent.name
+              parent_name = self.diagram_reader.database_diagram_members.tables_by_id[reference.start_point.target_object_id]
+              table_parent = self.diagram_reader.database_diagram_members.tables[parent_name]
+              relationship.parent_table = table_parent.name
 
-              table_child = self.diagram_reader.database_diagram_members.tables[reference.end_point.target_object_id]
-              #relationship.child_table = table_child.name
+              reference_column = table_parent.get_column_names()[reference.start_point.column_order]
+              relationship.referenced_key_column = reference_column
 
+              child_name = self.diagram_reader.database_diagram_members.tables_by_id[reference.end_point.target_object_id]
+              table_child = self.diagram_reader.database_diagram_members.tables[child_name]
+              relationship.child_table = table_child.name
 
+              foreign_column = table_child.get_column_names()[reference.end_point.column_order]
+              relationship.foreign_key_column = foreign_column
 
               database.relationships[database.relationships.length] = relationship
             end
